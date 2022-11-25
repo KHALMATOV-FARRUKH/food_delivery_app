15a26389eb18cfac68402bd3f33970936b16fa3d
Fara <115084566+KHALMATOV-FARRUKH@users.noreply.github.com>
2022-11-26 00:08:28 +0500

    Created folders: controllers data/     /api     /repository helper
    Created dart files:
    api_client.dart
    dependencies.dart
    popular_product_controller.dart
    popular_product_repo.dart

--- lib/pages/food/recommended_food_detail.dart
+++ lib/pages/food/recommended_food_detail.dart
@@ -4,6 +4,7 @@ import 'package:food_delivery_app/utils/colors.dart';
 import 'package:food_delivery_app/utils/dimensions.dart';
 import 'package:food_delivery_app/widgets/app_icon.dart';
 import 'package:food_delivery_app/widgets/big_text.dart';
+import 'package:food_delivery_app/widgets/expandable_text_widget.dart';
 
 class RecommendedFoodDetail extends StatelessWidget {
   const RecommendedFoodDetail({Key? key}) : super(key: key);
@@ -11,6 +12,7 @@ class RecommendedFoodDetail extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       body: CustomScrollView(
         slivers: [
           SliverAppBar(
@@ -51,8 +53,99 @@ class RecommendedFoodDetail extends StatelessWidget {
             ),
           ),
           SliverToBoxAdapter(
-            child: Text(
-                "- Cherry tomatoes 200 gr, \n- Chicken fillet 420 gr. \n- Parmesan cheese (or other hard cheese) 110 gr. \n- Iceberg salad 1 pc. \n- Loaf of 1 pc. \n- Olive oil 5 tspGarlic 2 clovesSalt to tasteGround black pepper to tasteFor the sauce: Chicken egg 2 pcs. \n- Mustard ½ tbspOlive oil 50 ml. \n- Garlic 2 cloves. \n- Parmesan cheese (or other hard cheese) 60 gr. \n- Lemon juice 2 tbspSalt to taste.  \n- Mustard ½ tbspOlive oil 50 ml. \n- Garlic 2 cloves. \n- Parmesan cheese (or other hard cheese) 60 gr. \n- Lemon juice 2 tbspSalt to taste. - Cherry tomatoes 200 gr, \n- Chicken fillet 420 gr. \n- Parmesan cheese (or other hard cheese) 110 gr. \n- Iceberg salad 1 pc. \n- Loaf of 1 pc. \n- Olive oil 5 tspGarlic 2 clovesSalt to tasteGround black pepper to tasteFor the sauce: Chicken egg 2 pcs. \n- Mustard ½ tbspOlive oil 50 ml. \n- Garlic 2 cloves. \n- Parmesan cheese (or other hard cheese) 60 gr. \n- Lemon juice 2 tbspSalt to taste.  \n- Mustard ½ tbspOlive oil 50 ml. \n- Garlic 2 cloves. \n- Parmesan cheese (or other hard cheese) 60 gr. \n- Lemon juice 2 tbspSalt to taste. - Cherry tomatoes 200 gr, \n- Chicken fillet 420 gr. \n- Parmesan cheese (or other hard cheese) 110 gr. \n- Iceberg salad 1 pc. \n- Loaf of 1 pc. \n- Olive oil 5 tspGarlic 2 clovesSalt to tasteGround black pepper to tasteFor the sauce: Chicken egg 2 pcs. \n- Mustard ½ tbspOlive oil 50 ml. \n- Garlic 2 cloves. \n- Parmesan cheese (or other hard cheese) 60 gr. \n- Lemon juice 2 tbspSalt to taste.  \n- Mustard ½ tbspOlive oil 50 ml. \n- Garlic 2 cloves. \n- Parmesan cheese (or other hard cheese) 60 gr. \n- Lemon juice 2 tbspSalt to taste.- Cherry tomatoes 200 gr, \n- Chicken fillet 420 gr. \n- Parmesan cheese (or other hard cheese) 110 gr. \n- Iceberg salad 1 pc. \n- Loaf of 1 pc. \n- Olive oil 5 tspGarlic 2 clovesSalt to tasteGround black pepper to tasteFor the sauce: Chicken egg 2 pcs. \n- Mustard ½ tbspOlive oil 50 ml. \n- Garlic 2 cloves. \n- Parmesan cheese (or other hard cheese) 60 gr. \n- Lemon juice 2 tbspSalt to taste.  \n- Mustard ½ tbspOlive oil 50 ml. \n- Garlic 2 cloves. \n- Parmesan cheese (or other hard cheese) 60 gr. \n- Lemon juice 2 tbspSalt to taste"),
+            child: Column(
+              children: [
+                Container(
+                  child: ExpandableTextWidget(
+                    text:
+                        "- Cherry tomatoes 200 gr, \n- Chicken fillet 420 gr. \n- Parmesan cheese (or other hard cheese) 110 gr. \n- Iceberg salad 1 pc. \n- Loaf of 1 pc. \n- Olive oil 5 tspGarlic 2 clovesSalt to tasteGround black pepper to tasteFor the sauce: Chicken egg 2 pcs. \n- Mustard ½ tbspOlive oil 50 ml. \n- Garlic 2 cloves. \n- Parmesan cheese (or other hard cheese) 60 gr. \n- Lemon juice 2 tbspSalt to taste.  \n- Mustard ½ tbspOlive oil 50 ml. \n- Garlic 2 cloves. \n- Parmesan cheese (or other hard cheese) 60 gr. \n- Lemon juice 2 tbspSalt to taste- Cherry tomatoes 200 gr, \n- Chicken fillet 420 gr. \n- Parmesan cheese (or other hard cheese) 110 gr. \n- Iceberg salad 1 pc. \n- Loaf of 1 pc. \n- Olive oil 5 tspGarlic 2 clovesSalt to tasteGround black pepper to tasteFor the sauce: Chicken egg 2 pcs. \n- Mustard ½ tbspOlive oil 50 ml. \n- Garlic 2 cloves. \n- Parmesan cheese (or other hard cheese) 60 gr. \n- Lemon juice 2 tbspSalt to taste.  \n- Mustard ½ tbspOlive oil 50 ml. \n- Garlic 2 cloves. \n- Parmesan cheese (or other hard cheese) 60 gr. \n- Lemon juice 2 tbspSalt to taste- Cherry tomatoes 200 gr, \n- Chicken fillet 420 gr. \n- Parmesan cheese (or other hard cheese) 110 gr. \n- Iceberg salad 1 pc. \n- Loaf of 1 pc. \n- Olive oil 5 tspGarlic 2 clovesSalt to tasteGround black pepper to tasteFor the sauce: Chicken egg 2 pcs. \n- Mustard ½ tbspOlive oil 50 ml. \n- Garlic 2 cloves. \n- Parmesan cheese (or other hard cheese) 60 gr. \n- Lemon juice 2 tbspSalt to taste.  \n- Mustard ½ tbspOlive oil 50 ml. \n- Garlic 2 cloves. \n- Parmesan cheese (or other hard cheese) 60 gr. \n- Lemon juice 2 tbspSalt to taste. - Cherry tomatoes 200 gr, \n- Chicken fillet 420 gr. \n- Parmesan cheese (or other hard cheese) 110 gr. \n- Iceberg salad 1 pc. \n- Loaf of 1 pc. \n- Olive oil 5 tspGarlic 2 clovesSalt to tasteGround black pepper to tasteFor the sauce: Chicken egg 2 pcs. \n- Mustard ½ tbspOlive oil 50 ml. \n- Garlic 2 cloves. \n- Parmesan cheese (or other hard cheese) 60 gr. \n- Lemon juice 2 tbspSalt to taste.  \n- Mustard ½ tbspOlive oil 50 ml. \n- Garlic 2 cloves. \n- Parmesan cheese (or other hard cheese) 60 gr. \n- Lemon juice 2 tbspSalt to taste. - Cherry tomatoes 200 gr, \n- Chicken fillet 420 gr. \n- Parmesan cheese (or other hard cheese) 110 gr. \n- Iceberg salad 1 pc. \n- Loaf of 1 pc. \n- Olive oil 5 tspGarlic 2 clovesSalt to tasteGround black pepper to tasteFor the sauce: Chicken egg 2 pcs. \n- Mustard ½ tbspOlive oil 50 ml. \n- Garlic 2 cloves. \n- Parmesan cheese (or other hard cheese) 60 gr. \n- Lemon juice 2 tbspSalt to taste.  \n- Mustard ½ tbspOlive oil 50 ml. \n- Garlic 2 cloves. \n- Parmesan cheese (or other hard cheese) 60 gr. \n- Lemon juice 2 tbspSalt to taste. - Cherry tomatoes 200 gr, \n- Chicken fillet 420 gr. \n- Parmesan cheese (or other hard cheese) 110 gr. \n- Iceberg salad 1 pc. \n- Loaf of 1 pc. \n- Olive oil 5 tspGarlic 2 clovesSalt to tasteGround black pepper to tasteFor the sauce: Chicken egg 2 pcs. \n- Mustard ½ tbspOlive oil 50 ml. \n- Garlic 2 cloves. \n- Parmesan cheese (or other hard cheese) 60 gr. \n- Lemon juice 2 tbspSalt to taste.  \n- Mustard ½ tbspOlive oil 50 ml. \n- Garlic 2 cloves. \n- Parmesan cheese (or other hard cheese) 60 gr. \n- Lemon juice 2 tbspSalt to taste- Cherry tomatoes 200 gr, \n- Chicken fillet 420 gr. \n- Parmesan cheese (or other hard cheese) 110 gr. \n- Iceberg salad 1 pc. \n- Loaf of 1 pc. \n- Olive oil 5 tspGarlic 2 clovesSalt to tasteGround black pepper to tasteFor the sauce: Chicken egg 2 pcs. \n- Mustard ½ tbspOlive oil 50 ml. \n- Garlic 2 cloves. \n- Parmesan cheese (or other hard cheese) 60 gr. \n- Lemon juice 2 tbspSalt to taste.  \n- Mustard ½ tbspOlive oil 50 ml. \n- Garlic 2 cloves. \n- Parmesan cheese (or other hard cheese) 60 gr. \n- Lemon juice 2 tbspSalt to taste",
+                  ),
+                  margin: EdgeInsets.only(
+                      left: Dimensions.width20, right: Dimensions.width20),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+      bottomNavigationBar: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Container(
+            padding: EdgeInsets.only(
+              left: Dimensions.width20 * 2.5,
+              right: Dimensions.width20 * 2.5,
+              top: Dimensions.height10,
+              bottom: Dimensions.height10,
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                AppIcon(
+                    iconSize: Dimensions.iconSize24,
+                    iconColor: Colors.white,
+                    backgroundColor: AppColors.mainColor,
+                    icon: Icons.remove),
+                BigText(
+                  text: "\$12.88 " + " X " + " 0 ",
+                  color: AppColors.mainBlackColor,
+                  size: Dimensions.font26,
+                ),
+                AppIcon(
+                    iconSize: Dimensions.iconSize24,
+                    iconColor: Colors.white,
+                    backgroundColor: AppColors.mainColor,
+                    icon: Icons.add),
+              ],
+            ),
+          ),
+          Container(
+            height: Dimensions.bottomHeightBar,
+            padding: EdgeInsets.only(
+                top: Dimensions.height30,
+                bottom: Dimensions.height30,
+                left: Dimensions.width20,
+                right: Dimensions.width20),
+            decoration: BoxDecoration(
+              color: AppColors.buttonBackgroundColor,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(Dimensions.radius20 * 2),
+                topRight: Radius.circular(Dimensions.radius20 * 2),
+              ),
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  padding: EdgeInsets.only(
+                      top: Dimensions.height20,
+                      bottom: Dimensions.height20,
+                      left: Dimensions.width20,
+                      right: Dimensions.width20),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(Dimensions.radius20),
+                    color: Colors.white,
+                  ),
+                  child: Icon(
+                    Icons.favorite,
+                    color: AppColors.mainColor,
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.only(
+                      top: Dimensions.height20,
+                      bottom: Dimensions.height20,
+                      left: Dimensions.width20,
+                      right: Dimensions.width20),
+                  child:
+                      BigText(text: "\$10 | Add to card", color: Colors.white),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(Dimensions.radius20),
+                    color: AppColors.mainColor,
+                  ),
+                ),
+              ],
+            ),
           ),
         ],
       ),
